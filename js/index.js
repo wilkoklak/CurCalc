3f0efadf91b5458693648912f8b9760f79c2ea70
Artur <thereaperv11@gmail.com>
2016-08-13 20:33:09 +0200

    animacja menu bocznego

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 var vars = {};
-var a_app = angular.module("mainApp", ["ngRoute"]);
+var a_app = angular.module("mainApp", ["ngRoute", "ngAnimate"]);
 a_app.run(function($rootScope) {
 	$rootScope.side_nav = false;
 	$rootScope.showSideNav = function() {
-		$rootScope.side_nav = true;;
+		var dark = document.querySelector(".dark");
+		var menu = document.querySelector(".side-nav");
+		if($rootScope.side_nav == false) {
+			menu.style.transform = "translateX(0)";
+			dark.style.animationName = "darken";
+			$rootScope.side_nav = true;
+		} else {
+			menu.style.transform = "translateX(-110%)";
+			dark.style.animationName = "lighten";
+			$rootScope.side_nav = false;
+		}
 	}
 	$rootScope.menu_items = [
 		{
