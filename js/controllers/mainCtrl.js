8f369ea8ad23a818642d493a30deaf0865485686
Artur <thereaperv11@gmail.com>
2016-08-13 22:27:51 +0200

    dodanie obsługi webstorage

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,7 +1,47 @@
 a_app.controller("mainCtrl", function($rootScope, $scope, $route, $http) {
 	$scope.waluty;
-	$http.get("http://api.nbp.pl/api/exchangerates/tables/A?format=json")
-	.then(function(response) {
-		$scope.waluty = response.data;
-	});
+	$scope.up_to_date = false;
+	function lsTest() {
+		var test = "test";
+		try {
+			window.localStorage.setItem(test, test);
+			window.localStorage.removeItem(test);
+			$scope.wsSupported = true;
+			return window.localStorage;
+		} catch(e) {
+			$scope.wsSupported = false;
+			return false;
+		}
+	}
+	$scope.storage = lsTest();
+	if($scope.wsSupported) {
+		if($scope.storage.waluty !== undefined) {
+			var json = JSON.parse($scope.storage.waluty);
+			var old = new Date(Date.parse(json[0].effectiveDate));
+			var now = new Date();
+			if(now.getMonth() - old.getMonth() == 0) {
+				old_day = old.getDate();
+				now_day = now.getDate();
+				if(now_day - old_day == 0) {
+					$scope.up_to_date = true;
+				} else {
+					if(now_day - old_day == 1 && (now.getDay() == 0 || now.getDay() == 6)) {
+						$scope.up_to_date = true;
+					} else {
+						$scope.up_to_date = false;
+					}
+				}
+			} else {
+				$scope.up_to_date = false;
+			}
+		}
+	}
+	if($scope.up_to_date == false) {
+		$http.get("http://api.nbp.pl/api/exchangerates/tables/A?format=json")
+		.then(function(response) {
+			$scope.waluty = response.data;
+
+			window.localStorage.waluty = JSON.stringify($scope.waluty);
+		});
+	}
 });
