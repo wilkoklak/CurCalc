ac9abe7b00f372f3c43e6cadd13b6ec8152749bf
Artur <thereaperv11@gmail.com>
2016-08-13 16:59:00 +0200

    test $http

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,3 +1,8 @@
-a_app.controller("mainCtrl", function($scope, $route) {
-	$scope.test = vars.test;
-})
+a_app.controller("mainCtrl", function($scope, $route, $http) {
+	$scope.waluty;
+	$http.get("http://api.nbp.pl/api/exchangerates/tables/A?format=json")
+	.then(function(response) {
+		$scope.waluty = response.data;
+		console.log($scope.waluty[0].code);
+	});
+});
--- js/index.js
+++ js/index.js
@@ -9,6 +9,6 @@ var app = {
 		document.addEventListener("deviceready", this.onDeviceReady, false);
 	},
 	onDeviceReady: function() {
-		alert(device.platform);
+		
 	}
 }
